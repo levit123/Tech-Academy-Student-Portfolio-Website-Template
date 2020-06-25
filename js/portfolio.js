8c055662916a57f77f818831fc3953a66871136f
levi <levicaseblodgett@gmail.com>
2020-06-24 17:30:51 -0700

    initial push
    - clean, modern look to new template
    - implements bootstrap components
    - javascript used to open links in new tabs
    - css styling various elements of the website (navbar, sections, background, image hover effects, etc.)
    - uses the 8 bit CSS font from googleapi
    - incomplete contact form using PHP (work in progress) (may be removed due to complexity for some students)
    
    Adjustments and additions to template to follow in future commits to this repository

---
+++ js/portfolio.js
@@ -0,0 +1,11 @@
+/*functions for opening links*/
+
+/*function that opens your Github page in a new tab*/
+function OpenGithub() {
+	window.open("https://github.com/levit123");
+}
+
+/*function that opens your LinkedIn profile in a new tab*/
+function OpenLinkedIn() {
+	window.open("https://www.linkedin.com/in/levi-blodgett-software/");
+}
