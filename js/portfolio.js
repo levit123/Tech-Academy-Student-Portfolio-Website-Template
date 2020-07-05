51828205c0cf0d6dd73879387f3e082ccb0a0b97
levi <levicaseblodgett@gmail.com>
2020-07-05 13:31:08 -0700

    Transferred and Cleaned From "changes" branch
    After downloading the other branch made by Tanner, made several changes including but not limited to:
    
    - Removing references to online Bootstrap and copying and adjusting snippets of Bootstrap CSS to CSS document for this specific project
    - Arranged images for the Projects section into a row
    - Removed 8 bit font
    - Re-organized and commented CSS
    - Adjusted font styling for all webpages
    - Tweaked flavor text in "index.html"
    - Removed PHP related code since that's not going to be implemented

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,32 +1,3 @@
-/*functions for the slideshow*/
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
 /*functions for opening links*/
 
 /*function that opens your Github page in a new tab*/
@@ -38,3 +9,4 @@ function OpenGithub() {
 function OpenLinkedIn() {
 	window.open("https://www.linkedin.com/in/levi-blodgett-software/");
 }
+
