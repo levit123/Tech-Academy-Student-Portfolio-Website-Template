548aba411f1c3e66e02b3ce3c8b27e63326aeb6e
tanner bergseng <tannermbergseng@gmail.com>
2020-07-09 18:09:56 -0700

    Pushing to new branch to avoid conflicts
    Added new pages for Projects along with slideshows

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,3 +10,41 @@ function OpenLinkedIn() {
 	window.open("https://www.linkedin.com/in/levi-blodgett-software/");
 }
 
+function OpenProject(){
+ window.open("Project1.html");
+}
+
+function OpenProject2(){
+	window.open("Project2.html");
+}
+
+function OpenProject3(){
+window.open("Project3.html");
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
