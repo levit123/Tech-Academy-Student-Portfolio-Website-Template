dd8f0dd2f2f84be174384f437b91c90fd1bf3f90
levi <levicaseblodgett@gmail.com>
2020-07-13 16:43:39 -0700

    combined two branches
    -combined changes from the Master branch and the "Changes-To-Merge" branch
    -manually checked to make sure everything is synced up and intact

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,15 +2,54 @@
 
 /*function that opens your Github page in a new tab*/
 function OpenGithub() {
-	window.open("https://github.com/levit123");
+  window.open("https://github.com/levit123");
 }
 
 /*function that opens your LinkedIn profile in a new tab*/
 function OpenLinkedIn() {
-	window.open("https://www.linkedin.com/in/levi-blodgett-software/");
+  window.open("https://www.linkedin.com/in/levi-blodgett-software/");
 }
 
 /*function that opens the Tech Academy website in a new tab*/
 function OpenTechAcademy() {
-	window.open("https://www.learncodinganywhere.com/");
+  window.open("https://www.learncodinganywhere.com/");
+}
+
+function OpenProject(){
+ window.open("Project1.html");
+}
+
+function OpenProject2(){
+	window.open("Project2.html");
+}
+
+function OpenProject3(){
+window.open("Project3.html");
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
