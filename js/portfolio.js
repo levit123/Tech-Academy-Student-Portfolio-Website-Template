f9c9242f9cd26035c8a6381cbbfed8d423052a5b
levi <levicaseblodgett@gmail.com>
2020-07-22 15:46:37 -0700

    comments, styling, and footer
    -pulled changes from previous push to Master branch made by Tanner
    -organized styling into sections
    -fixed issue with the navbar dropdown menu for "Live Projects" not displaying correctly
    -tweaked/cleaned up styling for various elements (navbar, anchor items, etc.)
    -replaced the "href" in the anchor element in the footer section with "onclick" that calls javascript function that opens the Tech Academy website in new tab
    -removed links to personal Github and LinkedIn, so students can enter their own links
    -added color and underline to "The Tech Academy" anchor element in footer section, so its more clear to the user that it's a click-able link
    -added comments to CSS classes and sections

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,30 +2,30 @@
 
 /*function that opens your Github page in a new tab*/
 function OpenGithub() {
-  window.open("https://github.com/levit123");
+  window.open("");
 }
 
 /*function that opens your LinkedIn profile in a new tab*/
 function OpenLinkedIn() {
-  window.open("https://www.linkedin.com/in/levi-blodgett-software/");
+  window.open("");
 }
 
 /*function that opens the Tech Academy website in a new tab*/
 function OpenTechAcademy() {
   window.open("https://www.learncodinganywhere.com/");
-}sss
+}
 
 /*function that opens your first favorite project on Github in a new tab*/
 function OpenProject(){
- window.open("https://github.com/levit123/MVC_CarInsurance_CodeFirst");
+ window.open("");
 }
 
 /*function that opens your second favorite project on Github in a new tab*/
 function OpenProject2(){
-	window.open("https://github.com/levit123/Basic-Python-Projects/tree/master/Tkinter%20GUI");
+	window.open("");
 }
 
 /*function that opens your third favorite project on Github in a new tab*/
 function OpenProject3(){
-window.open("https://github.com/levit123/Basic-Python-Projects/tree/master/Django%20Test%20Project/src");
+window.open("");
 }
